a3033ea3bf9f1ef2638fd1c6d04405121c0c7c4b
Rey Daniel L. Abing <reydanielabing322@gmail.com>
2023-10-28 00:08:23 +0800

    Test only 34 lines on index at repository

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+	<?php
+	include 'functions.php';
+    include 'validation.php';
+	?>
 <head>
     <title>Create User</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -8,10 +12,10 @@
 <div class="container">
 <div class="row justify-content-center">
 <div class="col-4">
-<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br>
 <h1> Create Menu</h1> 
 
-<form method="POST" action="addMenu.php">
+<form method="POST" enctype="multipart/form-data" action="index.php"> 
 
 <div class="form-group">
 	<label for="menuName">Menu Name</label>
@@ -29,6 +33,39 @@
 </div>
 </div>
 </div>
+</div>
+	<div>
+		<table class="table table-bordered table-hover mx-auto p-2" style="width:auto;">
+			<tr>
+				<td style="text-align: center;"><b>ID</b></td>
+				<td style="text-align: center;"><b>MENU NAME</b></td>
+				<td style="text-align: center;"><b>MENU DESCRIPTION</b></td>
+				<!-- <td style="text-align: center;"><b>PRICE</b></td> -->
+				<td style="text-align: center;"><b>ACTION</b></td>
+			</tr>
+			<?php
+			$rows = view_data();
+			foreach ($rows as $row) {
+				echo "<tr>";
+				echo "<td>" . $row['id'] . "</td>";
+				echo "<td>" . $row['menu_name'] . "</td>";
+				echo "<td>" . $row['menu_desc'] . "</td>";
+				
+			?>
+				<td>
+					<form method="post" enctype="multipart/form-data" action="?edit_id=<?php echo $row['id']; ?>" style="display: inline;">
+						<input type="hidden" name="edit" value="<?php echo $row['id']; ?>">
+						<button type="submit" >EDIT</button>&nbsp;
+					</form>
+
+					<form method="post" style="display: inline;">
+						<input type="hidden" name="delete" value="<?php echo $row['id']; ?>">
+						<button type="submit" onclick="return confirm('Delete record?')">DELETE</button>
+					</form>
+				</td>
+			<?php echo "</tr>";
+			}
+			?>
 
 </body>
 </html>
